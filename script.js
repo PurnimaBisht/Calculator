f27318643be193f4159ce09204583c88fb0e84d7
PurnimaBisht <135250637+PurnimaBisht@users.noreply.github.com>
2024-06-16 18:03:30 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-// Todo: Make M+ M- and MC functional
 let string = "";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {           // = 
+      if (string != "") {
+        string = eval(string);
+        document.querySelector('input').value = string;
+      }
     }
-    else if(e.target.innerHTML == 'C'){
+    else if (e.target.innerHTML == 'C') {      // C
       string = ""
       document.querySelector('input').value = string;
     }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
+    else if (e.target.innerHTML == 'CE') {     // CE
+      string = document.querySelector('input').value;
+      string = string.substring(0, string.length - 1);
+      document.querySelector('input').value = string;
+    }
+    else if (e.target.innerHTML == '.' || e.target.innerHTML == '-' || e.target.innerHTML == '+' ||
+      e.target.innerHTML == '*' || e.target.innerHTML == '/' || e.target.innerHTML == '**' ||
+      e.target.innerHTML == '%') {
+      string = document.querySelector('input').value;
+      let lastEntry = string.charAt(string.length - 1);
+      if (lastEntry == '.' || lastEntry == '-' || lastEntry == '+' || lastEntry == '*' ||
+        lastEntry == '/' || lastEntry == '**' || lastEntry == '%') {
+        document.querySelector('input').value = string;
       }
+      else {
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+      }
+    }
+    else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
   })
 })
